57318ebd83e0652bda19c4916c435b7118f97d34
wawanpi <muhaguskurniawan@gmail.com>
2023-07-01 02:19:52 +0700

    Make the left child of the new node point to null

--- binarysearchtree_202/binarysearchtree_202.cpp
+++ binarysearchtree_202/binarysearchtree_202.cpp
@@ -31,5 +31,6 @@ public:
 	{
 		Node* newNode = new Node(element, NULL, NULL);// Allocate memory for the new node
 		newNode->info = element; // Assign value to the data field of the new nod
+		newNode->leftchild = NULL; //Make the left child of the new node point to null
 	}
 };
