8bb1f333d936a56fc98630d0ea34d65dc750f740
wawanpi <muhaguskurniawan@gmail.com>
2023-07-01 02:50:41 +0700

    membuat suatu kondisi while

--- binarysearchtree_202/binarysearchtree_202.cpp
+++ binarysearchtree_202/binarysearchtree_202.cpp
@@ -56,5 +56,11 @@ public:
 	}
 
 
-	void
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		// This function searches the currentNoode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while
+	}
 };
