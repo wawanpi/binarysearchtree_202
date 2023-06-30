a6f2243a327943d149c40088e395ff2355d05ffe
wawanpi <muhaguskurniawan@gmail.com>
2023-07-01 03:00:06 +0700

    membuat methode preorder

--- binarysearchtree_202/binarysearchtree_202.cpp
+++ binarysearchtree_202/binarysearchtree_202.cpp
@@ -71,5 +71,19 @@ public:
 		}
 	}
 
-	void inorder()
+	void inorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
+	void
 };
